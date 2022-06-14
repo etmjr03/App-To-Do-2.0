c1ebda66cd132810eac0f231ba897c09661762c8
Edilson Teodoro Mathias Junior <12617082610@campus.machado.mch>
2022-06-13 21:22:58 -0300

    update app

---
+++ app/src/main/java/br/edu/ifsuldeminas/mch/tarefas2/domain/Category.java
@@ -0,0 +1,28 @@
+package br.edu.ifsuldeminas.mch.tarefas2.domain;
+
+public class Category {
+
+    private int id;
+    private String name;
+
+    public Category(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
